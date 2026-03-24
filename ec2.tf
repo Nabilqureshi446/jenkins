76b1e4b57bf4512275e96210d3b2994e6857988c
[Nabil Qureshi] <[nabilqureshi446@gmail.com]>
2026-03-24 17:09:24 +0530

    commit

--- ec2.tf
+++ ec2.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"   
 }
 
-# Key Pair (optional if you already have one)
-resource "aws_key_pair" "deployer" {
-  key_name   = "3tier"
-  public_key = file("C:/Users/hp/Downloads/3tier.pub")
-}
+# # Key Pair (optional if you already have one)
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "3tier"
+#   public_key = file("C:/Users/hp/Downloads/3tier.pub")
+# }
 
 # Security Group
 resource "aws_security_group" "my_sg" {
@@ -41,7 +41,7 @@ resource "aws_security_group" "my_sg" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0ec10929233384c7f"  # Amazon Linux 2 (ap-south-1)
   instance_type = "t3.small"
-  key_name      = aws_key_pair.deployer.key_name
+  key_name      = "3tier"  # Reference to the key pair created above
 
   security_groups = [aws_security_group.my_sg.name]
 
